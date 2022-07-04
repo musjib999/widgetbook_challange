b9f35fd1faf34eb3ab68f95dd26f872e7db28900
Musa Jibril <musjib999@gmail.com>
2022-07-04 21:20:33 +0100

    changes

--- lib/app.dart
+++ lib/app.dart
@@ -39,74 +39,89 @@ class _MyAppState extends State<MyApp> {
   static final RegExp nameRegExp = RegExp('[a-zA-Z]');
   final _formKey = GlobalKey<FormState>();
   final greet = Greet();
+
+  Future<void> greetUser() async {
+    try {
+      final response = await api.welcomeToWidgetbook(message: name.text);
+      greet.greetUser(response);
+    } on UnexpectedException catch (e) {
+      greet.greetError('An Error occured >>> ${e.toString()}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Observer(builder: (_) {
-      return Form(
-        key: _formKey,
-        child: Container(
-          margin: const EdgeInsets.all(15),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: name,
-                validator: (value) => value!.isEmpty
-                    ? 'Enter Your Name'
-                    : (nameRegExp.hasMatch(value)
-                        ? null
-                        : 'Name cannot be a number'),
-                decoration: const InputDecoration(
-                  hintText: 'Enter Name',
-                  prefixIcon: Icon(Icons.person_outline),
-                  border: OutlineInputBorder(),
+    return Observer(
+      builder: (_) {
+        return Form(
+          key: _formKey,
+          child: Container(
+            margin: const EdgeInsets.all(15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: name,
+                  validator: (value) => value!.isEmpty
+                      ? 'Enter Your Name'
+                      : (nameRegExp.hasMatch(value)
+                          ? null
+                          : 'Name cannot be a number'),
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Name',
+                    prefixIcon: Icon(Icons.person_outline),
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.text,
                 ),
-                keyboardType: TextInputType.text,
-              ),
-              const SizedBox(height: 15),
-              ArgonButton(
-                height: 40,
-                width: 180,
-                color: Colors.blue,
-                borderRadius: 8,
-                onTap: (startLoading, stopLoading, btnState) async {
-                  if (_formKey.currentState!.validate()) {
-                    try {
-                      final response =
-                          await api.welcomeToWidgetbook(message: name.text);
-                      greet.greetUser(response);
-                    } on UnexpectedException catch (e) {
-                      greet.greetUser('An Error occured >>> ${e.toString()}');
+                const SizedBox(height: 15),
+                ArgonButton(
+                  height: 40,
+                  width: 180,
+                  color: Colors.blue,
+                  borderRadius: 8,
+                  loader: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: const CircularProgressIndicator(
+                      color: Colors.white,
+                      // size: loaderWidth ,
+                    ),
+                  ),
+                  onTap: (startLoading, stopLoading, btnState) async {
+                    if (_formKey.currentState!.validate()) {
+                      startLoading();
+                      await greetUser();
+                      stopLoading();
                     }
-                  }
-                },
-                child: const Text(
-                  'Submit',
-                  style: TextStyle(color: Colors.white),
+                  },
+                  child: const Text(
+                    'Submit',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 15),
-              const Divider(),
-              const SizedBox(height: 15),
-              const Text(
-                'Greeting Message',
-                style: TextStyle(
-                  fontSize: 19,
-                  fontWeight: FontWeight.bold,
+                const SizedBox(height: 15),
+                const Divider(),
+                const SizedBox(height: 15),
+                const Text(
+                  'Greeting Message',
+                  style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                greet.message,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 19,
+                const SizedBox(height: 10),
+                Text(
+                  greet.message,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 19,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      );
-    });
+        );
+      },
+    );
   }
 }
